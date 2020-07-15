59d651996dae2db5ed0ca0c8db7294b22adde74f
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-15 19:46:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,10 +9,11 @@ end
 
 def reverse_each_word(phrase)
   array = phrase.split
+  reverse_array = []
   array.collect do |word|
-    word.reverse
-    phrase.join(" ")
+    reverse_array << word.reverse
   end
+  reverse_array.join(" ")
 end
 
 
