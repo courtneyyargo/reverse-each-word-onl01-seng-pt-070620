8cdbd3f13c1b36e5e18e88d70868112e72e93593
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-15 19:46:53 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+def reverse_each_word(phrase)
+  array = phrase.split
+  reverse_array = []
+  array.each do |word|
+    reverse_array << word.reverse
+  end
+  reverse_array.join(" ")
+end
+
+def reverse_each_word(phrase)
+  array = phrase.split
+  reverse_array = []
+  array.collect do |word|
+    reverse_array << word.reverse
+  end
+  reverse_array.join(" ")
+end
+
+
+#.collect will automatically creat a new array (get rid of reverse_array) and automatically return it but it won't join it for you in the end 
